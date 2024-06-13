ddd7966cb2e04c50096dbc7f0b52d03c77fa1b87
Aaliyah Iysha El Ghilani Fryatt <123201315+iysha22@users.noreply.github.com>
2024-06-13 16:54:01 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io"];
+  function generateDomainNames() {
+    for (let p of pronoun) {
+      for (let a of adj) {
+        for (let n of noun) {
+          for (let ext of extensions) {
+            console.log(`${p}${a}${n}${ext}`);
+          }
+          if (n.endsWith("us")) {
+            console.log(`${p}${a}${n.slice(0, -2)}.${n.slice(-2)}`);
+          }
+        }
+      }
+    }
+  }
+
+  generateDomainNames();
 };
